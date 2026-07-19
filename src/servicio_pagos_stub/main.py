f2778c8443b5fec8f36309a0a4adcbf7d2245aa9
whosstixi19 <tixi4615@gmail.com>
2026-07-18 20:55:12 -0500

    servicio_pagos_stub: latencia de 20s condicional

--- src/servicio_pagos_stub/main.py
+++ src/servicio_pagos_stub/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
 import asyncio
 
-app = FastAPI(title="Stub de Pagos (Caótico)")
+app = FastAPI(title="Stub de Pagos Caótico")
 
 class PagoRequest(BaseModel):
     monto: float
 
 @app.post("/pagar")
 async def pagar(pago: PagoRequest):
-    # CAOS: Simulamos latencia extrema en la red externa
-    print("[STUB-PAGOS] Procesando transacción lenta de pago...")
-    await asyncio.sleep(20.0) # Esto forzará los timeouts del llamador
+    latencia_activa = os.getenv("LATENCIA_ACTIVA", "false").lower() == "true"
     
-    return {
-        "status": "success",
-        "transaccion_id": "tx_fake_uuid_12345",
-        "monto_procesado": pago.monto
-    }
+    if latencia_activa:
+        print("[CAOS ACTIVADO] Aplicando retraso artificial de 20 segundos.")
+        await asyncio.sleep(20.0)
+        
+    return {"status": "success", "transaccion_id": "tx_k8s_resilient", "monto_procesado": pago.monto}
